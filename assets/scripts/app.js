ebf661176b1ec17fe51e4a6808481c3bf2157391
yocosaka <sakayoco55@gmail.com>
2021-02-03 12:21:02 +0900

    Add all necessary fields

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,69 +1,28 @@
 // At the beginning
-// import { func_name1, func_name2 } from './functions';
+import {
+  Book, addBookToLibrary, displayBooks,
+} from './functions';
 
+// ARRAY FOR LIBRARY
 const myLibrary = [];
 
-function Book(title, author, year, status = false) {
-  this.title = title;
-  this.author = author;
-  this.year = year;
-  this.status = status;
-}
+// RUN
+displayBooks(myLibrary);
 
-function addBookToLibrary(book) {
-  book = {
-    title: book.title,
-    author: book.author,
-    year: book.year,
-    status: book.status,
-  };
-  myLibrary.push(book);
-}
-
-function bookStatus(book) {
-  return book.status === true ? 'Read Already' : 'Not yet';
-}
-
-const books = document.getElementById('books_body');
-function displayBooks(library) {
-  books.innerHTML = '';
-  library.forEach((book, index) => {
-    const tr = document.createElement('tr');
-    books.appendChild(tr);
-
-    tr.innerHTML = `
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.year}</td>
-      <td>${bookStatus(book)}</td>
-    `;
-
-    const rmBtn = document.createElement('button');
-    rmBtn.setAttribute('type', 'button');
-    rmBtn.setAttribute('data-target', index);
-    rmBtn.classList.add('remove-btn');
-    rmBtn.innerText = 'Remove';
-    const td = document.createElement('td');
-    tr.appendChild(td);
-    td.appendChild(rmBtn);
-
-    const rmBtns = document.querySelectorAll('.remove-btn');
-    rmBtns.forEach(btn => {
-      btn.addEventListener('click', e => {
-        e.preventDefault();
-        myLibrary.splice(e.target.getAttribute('data-target'), 1);
-        displayBooks(myLibrary);
-      });
-    });
-  });
-}
-
+// GET ELEMENTS FROM HTML
 const form = document.querySelector('#add_form');
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
 const yearInput = document.getElementById('year');
 const statusInput = document.getElementById('status');
+const newBookBtn = document.querySelector('#new_book');
 
+// TOGGLE BUTTON FOR FORM
+newBookBtn.addEventListener('click', () => {
+  form.classList.toggle('open');
+});
+
+// FORM SUBMISSION
 form.addEventListener('submit', e => {
   e.preventDefault();
   const title = e.target.elements.title.value;
@@ -72,16 +31,11 @@ form.addEventListener('submit', e => {
   const status = e.target.elements.status.checked;
 
   const newBook = new Book(title, author, year, status);
-  addBookToLibrary(newBook);
+  addBookToLibrary(newBook, myLibrary);
   displayBooks(myLibrary);
 
   titleInput.value = '';
   authorInput.value = '';
   yearInput.value = '';
   statusInput.checked = false;
-});
-
-const newBookBtn = document.querySelector('#new_book');
-newBookBtn.addEventListener('click', () => {
-  form.classList.toggle('open');
 });
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,4 @@
+//  FUNCTIONS =======================================
 function Book(title, author, year, status = false) {
   this.title = title;
   this.author = author;
@@ -56,6 +57,41 @@ function displayBooks(library) {
   });
 }
 
-export {
-  Book, addBookToLibrary, displayBooks,
-};
+//  RUNNING =======================================
+
+// ARRAY FOR LIBRARY
+const myLibrary = [];
+
+// RUN
+displayBooks(myLibrary);
+
+// GET ELEMENTS FROM HTML
+const form = document.querySelector('#add_form');
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const yearInput = document.getElementById('year');
+const statusInput = document.getElementById('status');
+const newBookBtn = document.querySelector('#new_book');
+
+// TOGGLE BUTTON FOR FORM
+newBookBtn.addEventListener('click', () => {
+  form.classList.toggle('open');
+});
+
+// FORM SUBMISSION
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const title = e.target.elements.title.value;
+  const author = e.target.elements.author.value;
+  const year = e.target.elements.year.value;
+  const status = e.target.elements.status.checked;
+
+  const newBook = new Book(title, author, year, status);
+  addBookToLibrary(newBook, myLibrary);
+  displayBooks(myLibrary);
+
+  titleInput.value = '';
+  authorInput.value = '';
+  yearInput.value = '';
+  statusInput.checked = false;
+});
